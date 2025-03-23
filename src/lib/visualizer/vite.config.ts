778cb979004a3f90afb3bd33a1d1f7234d005663
Rafał Majewski <goodheropl@gmail.com>
2025-03-23 21:02:19 +0100

    feature: Add existing files

--- src/lib/visualizer/vite.config.ts
+++
@@ -1,6 +0,0 @@
-import {sveltekit as createSvelteKitPlugins} from "@sveltejs/kit/vite";
-import type {UserConfig} from "vite";
-const svelteKitPlugins = await createSvelteKitPlugins();
-export default {
-	plugins: svelteKitPlugins,
-} as const satisfies UserConfig;
